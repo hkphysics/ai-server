291afd3a47fe67068fd76701f25ceb963ee01040
Joseph C Wang <joequant@gmail.com>
2026-05-29 13:28:48 +0000

    fix user to .env

---
+++ jupyter/jupyterhub_config.py
@@ -0,0 +1,16 @@
+import os
+c.JupyterHub.ip = 'jupyter'
+c.JupyterHub.hub_connect_ip = 'jupyter'
+c.JupyterHub.bind_url = 'http://jupyter:8000/'
+c.Authenticator.admin_users = os.environ['ADMIN_USERS'].split(',')
+c.Authenticator.allowed_users = os.environ['ALLOWED_USERS'].split(',')
+c.JupyterHub.authenticator_class = 'oauthenticator.google.LocalGoogleOAuthenticator'
+c.LocalGoogleOAuthenticator.create_system_users=True
+c.LocalGoogleOAuthenticator.client_id = os.environ['GOOGLE_CLIENT_ID']
+c.LocalGoogleOAuthenticator.client_secret = os.environ['GOOGLE_CLIENT_SECRET']
+c.LocalGoogleOAuthenticator.oauth_callback_url = os.environ['GOOGLE_OAUTH_CALLBACK_URL']
+c.LocalGoogleOAuthenticator.auto_login = True
+c.LocalGoogleOAuthenticator.add_user_cmd = ['adduser', '-q', '--gecos', '""', '--disabled-password', '--allow-bad-names']
+c.Cull.timeout = 21600
+c.Spawner.start_timeout = 300
+c.Spawner.http_timeout = 300
